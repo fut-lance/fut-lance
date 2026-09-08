ad337feb2df844543bc7b314b29c1011a4320ef3
Rafael Melegari de Souza <rafaelmelegari86@gmail.com>
2026-09-07 21:49:04 -0300

    fix: use documentId for routing, fix category filter, rename /noticia to /noticias

--- src/app/categoria/[slug]/page.tsx
+++ src/app/categoria/[slug]/page.tsx
@@ -52,7 +52,7 @@ export default async function CategoriaPage({
           {noticias.map((noticia: any) => (
             <CardNoticia
               key={noticia.id}
-              slug={noticia.slug}
+              slug={noticia.documentId}
               titulo={noticia.titulo}
               resumo={noticia.resumo}
               imagem={noticia.imagem_capa?.url ? `${apiUrl}${noticia.imagem_capa.url}` : 'https://images.unsplash.com/photo-1522778119026-d647f0596c20?w=800'}

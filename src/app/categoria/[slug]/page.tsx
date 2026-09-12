53c557f3a28805e00d607afd84c5f286af9bfa45
Rafael Melegari de Souza <rafaelmelegari86@gmail.com>
2026-09-11 21:41:52 -0300

    chore: clean cache-bust hack

--- src/app/categoria/[slug]/page.tsx
+++ src/app/categoria/[slug]/page.tsx
@@ -139,7 +139,6 @@ export default async function CategoriaPage({
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div data-cache-bust={Date.now()} className="hidden" />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(categorySchema) }}
